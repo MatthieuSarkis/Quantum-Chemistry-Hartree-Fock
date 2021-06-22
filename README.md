a8722ae4d8ac81101728d258a42c7a8783e5d066
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-06-22 15:00:31 +0900

    basic skeleton of the project

---
+++ README.md
@@ -0,0 +1,19 @@
+# Hartree-Fock algorithm
+
+## Requirements
+
+* python>=3.6
+* numpy
+* jupyter
+
+```shell
+pip install -r requirements.txt
+python setup.py install
+```
+ ## Examples 
+ 
+## License
+[Apache License 2.0]()
+
+
+## To do
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='Hartree-Fock algorithm',
+   version='1.0',
+   description='',
+   license="Apache License 2.0",
+   author='Matthieu Sarkis',
+   author_email='matthieu.sarkis@gmail.com',
+   packages=['src'],
+)
