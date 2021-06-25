98f095c491952d7575717ee6d81ac7e8261d85d8
Matthieu Sarkis <matthieu.sarkis@gmail.com>
2021-06-25 20:14:06 +0900

    added info.py file

--- src/atom.py
+++ src/atom.py
@@ -11,22 +11,9 @@
 # that they have been altered from the originals.
 
 from typing import List
-from src.atomic_orbital import AtomicOrbital    
+from src.atomic_orbital import AtomicOrbital 
+from src.info import * 
 class Atom():
-    
-    zeta = {'H' : [1.24], 'He': [2.0925], 'Li': [2.69, 0.80]}
-    maximal_orbital_quantum_number = {'H' : 1, 'He': 1, 'Li': 2}
-    charge = {'H' : 1, 'He': 2, 'Li': 3}
-    
-    ### STOnG basis ###
-    weights_STOnG = {'H' :[[0.444635, 0.535328, 0.154329], [0.700115, 0.399513, -0.0999672]]}
-    exponents_STOnG = {'H' : [[0.109818, 0.405771, 2.22766], [0.0751386, 0.231031, 0.994203]]} 
-    ### STO-3G basis ###
-    weights_STO_3G = {'H' :[[0.1543289673E+00, 0.5353281423E+00, 0.4446345422E+00]]}
-    exponents_STO_3G = {'H' : [[0.3425250914E+01, 0.6239137298E+00, 0.1688554040E+00]]}
-    ### 6-31G basis ###
-    weights_6_31G = {'H' :[[0.3349460434E-01, 0.2347269535E+00, 0.8137573261E+00], [1.0000000]]}
-    exponents_6_31G = {'H' : [[0.1873113696E+02, 0.2825394365E+01, 0.6401216923E+00], [0.1612777588E+00]]}
     
     def __init__(self,
                  atom_type: str,
@@ -35,20 +22,20 @@ class Atom():
                  ) -> None:
         
         self.atom_type = atom_type
-        self.charge = Atom.charge[self.atom_type]
-        self.zeta_scaling = Atom.zeta[self.atom_type]
+        self.charge = charge[self.atom_type]
+        self.zeta_scaling = zeta[self.atom_type]
         self.coordinates = coordinates
         self.number_atomic_orbitals = len(self.zeta_scaling)
         
         if basis == 'STOnG':
-            self.basis_weights = Atom.weights_STOnG[atom_type]
-            self.basis_exponents = Atom.exponents_STOnG[atom_type]
+            self.basis_weights = weights_STOnG[atom_type]
+            self.basis_exponents = exponents_STOnG[atom_type]
         elif basis == 'STO-3G':
-            self.basis_weights = Atom.weights_STO_3G[atom_type]
-            self.basis_exponents = Atom.exponents_STO_3G[atom_type]
+            self.basis_weights = weights_STO_3G[atom_type]
+            self.basis_exponents = exponents_STO_3G[atom_type]
         elif basis == '6-31G':
-            self.basis_weights = Atom.weights_6_31G[atom_type]
-            self.basis_exponents = Atom.exponents_6_31G[atom_type]
+            self.basis_weights = weights_6_31G[atom_type]
+            self.basis_exponents = exponents_6_31G[atom_type]
             
         self.atomic_orbitals = [AtomicOrbital(basis_weights=self.basis_weights[i],
                                               basis_exponents=self.basis_exponents[i],
